19428b9869d58982791bb5c6ae9f3086fb750dc6
Lord Vollkorn <bertozzi@i-ways.de>
2020-03-16 17:06:19 +0100

    worksform

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# magento2-paypal-instalments-banners
+# Magento 2 PaypalInstalmentsBanner module
+
+```
+.
+├── Helper
+│   └── Data.php
+├── etc
+│   ├── adminhtml
+│   │   └── system.xml
+│   └── module.xml
+├── i18n
+│   └── de_DE.csv
+├── README.md
+├── composer.json
+└── registration.php
+```
---
+++ registration.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magentocommerce.com and you will be sent a copy immediately.
+ *
+ * PHP version 7.2.18
+ *
+ * @category Modules
+ * @package  Magento
+ * @author   Bertozzi Matteo <bertozzi@i-ways.net>
+ * @license  http://opensource.org/licenses/osl-3.0.php Open Software License 3.0
+ * @link     https://www.i-ways.net
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Iways_PaypalInstalmentsBanners',
+    __DIR__
+);
