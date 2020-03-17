f872e68e951a66ce1a492fb78a89bf28a4d70bf9
Lord Vollkorn <bertozzi@i-ways.de>
2020-03-17 09:45:54 +0100

    worksforms

---
+++ Model/System/Config/Source/Banner/Color.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magentocommerce.com and you will be sent a copy immediately.
+ *
+ * PHP version 7.2.18
+ *
+ * @category Modules
+ * @package  Magento
+ * @author   Bertozzi Matteo <bertozzi@i-ways.net>
+ * @license  http://opensource.org/licenses/osl-3.0.php Open Software License 3.0
+ * @link     https://www.i-ways.net
+ */
+
+/**
+ * Iways\PaypalInstalmentsBanners\Model\System\Config\Source\Banner\Color
+ *
+ * @category Models
+ * @package  Magento
+ * @author   Bertozzi Matteo <bertozzi@i-ways.net>
+ * @license  http://opensource.org/licenses/osl-3.0.php Open Software License 3.0
+ * @link     https://www.i-ways.net
+ */
+
+namespace Iways\PaypalInstalmentsBanners\Model\System\Config\Source\Banner;
+
+class Color implements \Magento\Framework\Option\ArrayInterface
+{
+    const NO_BANNER = 0,
+          COLOR_WHITE = 'white',
+          //COLOR_WHITE_NO_BORDER = 'white-no-border',
+          COLOR_GRAY = 'gray',
+          COLOR_BLUE = 'blue',
+          COLOR_BLACK = 'black';
+
+    /**
+     * Translated configuration options
+     *
+     * @return array
+     */
+    public function toArray()
+    {
+        return [
+            self::NO_BANNER => __('none'),
+            self::COLOR_WHITE => __('white'),
+            self::COLOR_GRAY => __('gray'),
+            self::COLOR_BLUE => __('blue'), // default, see etc/config.xml
+            self::COLOR_BLACK => __('black')
+        ];
+    }
+
+    /**
+     * Translated configuration options in multiarray
+     *
+     * @return array
+     */
+    public function toOptionArray()
+    {
+        $output = [];
+
+        foreach ($this->toArray() as $key => $value) {
+            $output[] = [
+                'value' => $key,
+                'label' => $value
+            ];
+        }
+
+        return $output;
+    }
+}
--- README.md
+++ README.md
@@ -4,9 +4,17 @@
 .
 ├── Helper
 │   └── Data.php
+├── Model
+│   └── System
+│       └── Config
+│           └── Source
+│               └── Banner
+│                   └── Color.php
 ├── etc
 │   ├── adminhtml
 │   │   └── system.xml
+│   ├── acl.xml
+│   ├── config.xml
 │   └── module.xml
 ├── i18n
 │   └── de_DE.csv
